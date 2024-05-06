afb898c1c37866a6a358149514c511e16c04df58
kajj8808 <a01039388808@gmail.com>
2024-05-07 05:45:04 +0900

    ✅ 제목이 2줄 이상이 될 경우 수정

--- components/series-modal.tsx
+++ components/series-modal.tsx
@@ -68,14 +68,14 @@ export default function SeriesModal({
                 </div>
                 <div className="flex flex-col justify-center col-span-8">
                   <div className="flex justify-between">
-                    <h4 className="text-xs font-bold text-product-color lg:text-base">
+                    <h4 className="text-xs font-bold text-product-color lg:text-base line-clamp-1">
                       아비도스 고등학교 폐교 대책위원회
                     </h4>
                     <h5 className="text-xs font-bold text-product-color lg:text-base">
                       24m
                     </h5>
                   </div>
-                  <h5 className="text-xs sm:mt-2 md:mt-4 line-clamp-2 text-product-color lg:text-sm">
+                  <h5 className="mt-1 text-xs sm:mt-2 md:mt-4 line-clamp-1 sm:line-clamp-2 text-product-color lg:text-sm">
                     재정 문제로 폐쇄 직전에 있는 아비도스 고등학교. 그리고
                     학교를 살리기 위해 고군분투하는 대책위원회가 있다.
                     대책위원회의 위원장인 호시노를 비롯한 아이들은 오늘도 학교를
