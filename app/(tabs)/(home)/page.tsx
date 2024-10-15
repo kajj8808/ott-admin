666f682ee87055a47ba7845802994ddf3cc1f38e
kajj8808 <a01039388808@gmail.com>
2024-10-15 22:29:29 +0900

    ✅ 임시 수정

--- app/(tabs)/(home)/page.tsx
+++ app/(tabs)/(home)/page.tsx
@@ -3,13 +3,22 @@ import { getNowPlayingSeries } from "./actions";
 import { unstable_cache as nextCache } from "next/cache";
 import Slider from "@/components/framer-slider";
 import ProductSlider from "@/components/product-slider";
+import db from "@/lib/db";
+import Image from "next/image";
+import Link from "next/link";
 
 const getChachedNowPlaying = nextCache(getNowPlayingSeries, ["now-playing"], {
   revalidate: 60,
 });
 
+const getSeries = async () => {
+  const series = await db.series.findMany();
+  return series;
+};
+
 export default async function Page() {
   const nowPlayingSeries = await getChachedNowPlaying();
+  const allSeries = await getSeries();
   /* const seriesIn2024Q4 = await getSeriesByDateRange(
     new Date(2024, 7, 2),
     new Date(2024, 9, 30)
@@ -21,9 +30,18 @@ export default async function Page() {
         Now playing
       </h3>
       <ProductSlider items={nowPlayingSeries} />
-      {/* <Slider /> */}
-      {/*       <ProductSlider series={seriesIn2024Q4} />
-       */}{" "}
+      <div className="grid grid-cols-6 gap-px p-3">
+        {allSeries.map((series) => (
+          <Link key={series.id} href={`/series/${series.id}`}>
+            <Image
+              alt={series.title}
+              src={series.cover_image}
+              width={342}
+              height={192}
+            />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
