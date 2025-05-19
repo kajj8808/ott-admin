8256e2502ee4f6c441aca56fec5d76532f644251
kajj8808 <a01039388808@gmail.com>
2025-05-19 23:49:33 +0900

    ✅ admin page에 host name 하나 더 추가

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "image.tmdb.org" }],
+    remotePatterns: [
+      { protocol: "https", hostname: "image.tmdb.org" },
+      { hostname: "kajj8808.com" },
+    ],
   },
 };
 
